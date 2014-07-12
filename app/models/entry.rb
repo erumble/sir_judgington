3407bcfbef17bf0d9c01c3d52678e66a8ed9f92b
Eric Rumble <eric.rumble@sage.com>
2014-07-11 22:55:49 -0400

    add some validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -8,4 +8,5 @@ class Entry < ActiveRecord::Base
   has_and_belongs_to_many :categories, -> { readonly }
   has_and_belongs_to_many :contestants
   belongs_to :contest
+  validates :contest, presence: true
 end
