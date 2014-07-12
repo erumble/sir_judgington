2d8af851d7cf1d6f0d8b7d64ae9f7afa07fca105
Alan Davis <alan.davis@sage.com>
2014-07-11 23:39:31 -0400

    fixed bad testing practice

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 
-
   resources :entries
 
   unless Rails.application.config.consider_all_requests_local
--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -1,19 +1,18 @@
 require "rails_helper"
 
 describe EntriesController, type: :controller do
-  subject(:index) { get :index, {  } }
-  subject(:new) { get :new, {  } }
-  subject(:create) { post :create, {  } }
-
   describe 'index' do
-    it { expect(index).to be_success }
+    subject { get :index, {  } }
+    it { is_expected.to be_success }
   end
 
   describe 'new' do
-    it { expect(new).to be_success }
+    subject { get :new, {  } }
+    it { is_expected.to be_success }
   end
 
   describe 'create' do
-    it { expect(create).to be_redirect }
+    subject { post :create, {  } }
+    it { is_expected.to be_redirect }
   end
 end
