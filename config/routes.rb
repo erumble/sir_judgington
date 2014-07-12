20c49ffafdf1aa77c6133aca34c860239019c528
Joshua Brewton <Joshua.Brewton@sage.com>
2014-07-11 22:17:42 -0400

    Non local error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+
+  unless Rails.application.config.consider_all_requests_local
+    match '*not_found', to: 'errors#error_404', via: [:get, :post]
+  end
 end
