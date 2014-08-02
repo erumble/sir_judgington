f04237a31d07576c7439af8875c319b85d0bfc03
Eric Rumble <eric.rumble@sage.com>
2014-08-02 18:25:38 -0400

    hooked up new models

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,11 +6,19 @@ class EntriesController < ApplicationController
 
   def new
     @entry = Entry.new
-    @contestant = Contestant.new
+    @person = Person.new
+    # @character = Character.new
+    # @cosplay = Cosplay.new(entry: @entry, owner: @person, character: @character)
   end
 
   def create
+    @person = Person.where(params[:person].to_h).first_or_create
+
+    @character = Character.where(params[:character].to_h).first_or_create
+
     @entry = Entry.create! params[:entry].to_h
+
+    @cosplay = Cosplay.create!(owner: @person, entry: @entry, character: @character)
 
     redirect_to entries_path
   end
