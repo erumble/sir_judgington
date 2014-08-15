6dcb1060db752a42e79faec0ad7a26c6f4b8ea6b
Joshua Brewton <Joshua.Brewton@sage.com>
2014-08-15 12:27:01 -0400

    updates to entries to save

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,48 +6,45 @@ class EntriesController < ApplicationController
 
   def new
     @entry = Entry.new
-
-    3.times do
-      cosplay = Cosplay.new
-      owner = Person.new
-      character = Character.new
-      cosplay.owner = owner
-      cosplay.character = character
-      @entry.cosplays << cosplay
-    end
+    @entry.contest = Contest.last
+    cp = @entry.cosplays.build
+    cp.build_owner
+    cp.build_character
   end
 
   def create
     begin
       @entry = Entry.create! entry_params
-
-      person = Person.where(person_params).first_or_create
-
-      character = Character.where(character_params).first_or_create
-
-      Cosplay.create!(owner: person, entry: @entry, character: character)
-
+      cosplay_params[:cosplays_attributes].each do |k, cosplay|
+        owner =  Person.where(cosplay[:owner_attributes]).first_or_create
+        character = Character.where(cosplay[:character_attributes]).first_or_create
+        Cosplay.create!(owner: owner, entry: @entry, character: character)
+      end
       redirect_to entries_path
     rescue => e
-      flash[:error] = "There was an error saving the entry."
+      flash[:error] = "There was an error saving the entry. #{e.message}"
+      session[:entry_create_data] = @entry
       redirect_to :back
     end
   end
 
-
   private
 
-  def person_params
-    params.require(:person).permit(:first_name, :last_name, :phonetic_spelling, :email)
-  end
-
-  def character_params
-    params.require(:character).permit(:name, :property)
+  def cosplay_params
+    params.require(:entry).permit(:cosplays_attributes => [owner_attributes: [:first_name, :last_name, :phonetic_spelling, :email], character_attributes: [:name, :property]])
   end
 
   def entry_params
     # contest id should not be permitted, it should be set by the system
-    params.require(:entry).permit(:judging_time_id, :contest_id, :skill_level, :hot_or_bulky?, :group_name, :handler_count)
+    params.require(:entry).permit(
+      :judging_time_id,
+      :contest_id,
+      :skill_level,
+      :hot_or_bulky?,
+      :group_name,
+      :handler_count,
+      :category_ids => []
+    )
   end
 
 end
