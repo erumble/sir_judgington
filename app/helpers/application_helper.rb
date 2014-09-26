3b7a0fafa2c6cfa2e8274a2dea9d01479681fd97
Joshua Brewton <Joshua.Brewton@sage.com>
2014-09-26 05:41:32 -0400

    pressing enter does weird... we got two way suggestions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,13 +13,13 @@ module ApplicationHelper
   end
 
   def trashcan_button
-    # raw("<span class='fa-stack fa-lg'><i class='fa fa-square-o fa-stack-2x'></i><i class='fa fa-lg fa-trash-o fa-stack-1x'></i></span>")
-    raw("<button class='btn btn-primary'><i class='fa fa-lg fa-trash-o'></i></button>")
+    raw("<span class='fa-stack fa-lg'><i class='fa fa-square-o fa-stack-2x'></i><i class='fa fa-lg fa-trash-o fa-stack-1x'></i></span>")
+    # raw("<button class='btn btn-primary'><i class='fa fa-lg fa-trash-o'></i></button>")
   end
 
   def plus_button
-    # raw("<span class='fa-stack fa-lg'><i class='fa fa-square-o fa-stack-2x'></i><i class='fa fa-lg fa-plus fa-stack-1x'></i> </span>")
-    raw("<button class='btn btn-primary'><i class='fa fa-lg fa-plus'></i></button>")
+    raw("<span class='fa-stack fa-lg'><i class='fa fa-square-o fa-stack-2x'></i><i class='fa fa-lg fa-plus fa-stack-1x'></i> </span>")
+    # raw("<button class='btn btn-primary'><i class='fa fa-lg fa-plus'></i></button>")
   end
 
   def edit_button
