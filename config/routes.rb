43b326a40b34f15f6e32de0a60558d6df19aa278
Joshua Brewton <Joshua.Brewton@sage.com>
2014-09-27 13:06:50 -0400

    basic csv view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
   match 'entries/person_json_blob', via: [:get]
   match 'entries/character_json_blob', via: [:get]
+  match 'entries/csv', via: [:get]
   resources :entries do
 
   end
